6c2c4df9a6e7ca31ab593a488a39c10db38841b8
Michael Wimble <mike@wimblerobotics.com>
2024-11-24 21:03:29 -0800

    Renaming things, adding map layer content

---
+++ description_1/scripts/1.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+ros2 launch description_1 description.launch.py urdf_file_name:="1.urdf"
---
+++ description_2/scripts/3.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+ros2 launch description_2 description.launch.py urdf_file_name:="3.urdf"
